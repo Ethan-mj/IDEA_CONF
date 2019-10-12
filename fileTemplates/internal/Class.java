5367abd0bd5397346b1c964d57853ffaa24d5acc
ethan <ethan.zhang@zeasn.com>
2019-10-12 11:06:56 +0800

    IU-2018.1.8 <Tiny@LAPTOP-EV0TE80G Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, print.xml, gvim.icls, project.default.xml, other.xml, Default.xml, ignore.xml, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, Ladies Night 2.icls, javarebel.xml, Leone Dark 2.icls, Ladies Night 5.icls, Default.xml, laf.xml, Class.java, jdk.table.xml, debugger.renderers.xml, path.macros.xml, proxy.settings.xml, vcs.xml, editor.codeinsight.xml, databaseSettings.xml, toStringTemplates.xml, editor.xml, Solarized.icls, debugger.xml, RecognEyes.icls, sonarlint.xml

---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * @author Ethan
+ * @date ${YEAR}-${MONTH}-${DAY}
+ * @description ${description}
+ */
+public class ${NAME} {
+}
